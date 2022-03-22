e66b2e01ee234b4a5f8376704eb0c17cc319c31d
Patrick.James.Brennan.The.Third <patrick.james.brennan.three@gmail.com>
2022-03-22 15:50:49 -0400

    .

--- MLGproject/main.cpp
+++ MLGproject/main.cpp
@@ -7,9 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    cout<<"hello"<<endl;
-    cout<<"hello2"<<endl;
-    cout<<"hello3"<<endl;
     return a.exec();
 
 }
